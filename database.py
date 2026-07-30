835008012b0c7b66a7a9206bcadedb44641fab78
Bontha Sravanthi <bonthasravanthi850@gmail.com>
2026-07-30 21:09:17 +0530

    Change database connection from MySQL to PostgreSQL

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 #to create connection with database
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:sravanthi%401234@localhost:3306/product_db"
+# DATABASE_URL = "mysql+pymysql://root:sravanthi%401234@localhost:3306/product_db"
+DATABASE_URL = "postgres://avnadmin:AVNS_YebMrQt0bjL6T9VAgut@pg-17488285-bonthasravanthi850-750a.k.aivencloud.com:19191/defaultdb?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
